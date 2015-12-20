3806e65e952b7edef251b6cbcef0fe946d116168
Jonathan Davis <jonathanherscheldavis@gmail.com>
2015-12-19 19:08:29 -0500

    finished the functions and events on the jq

--- jqcheatsheet.js
+++ jqcheatsheet.js
@@ -1,8 +1,23 @@
 $(document).ready(
 	function() {
-		$("h1").animate({fontSize: "75px"}, 3000)
-	});
+		$("h1").animate({fontSize: "75px"}, 2000);
+	}
+	);
 //grows when page loads
+
+$(document).ready(
+	function() {
+		$("#goodbye").fadeOut(6000);
+	}
+	);
+//fades slowly over 6 seconds when page loads
+
+$("#slide").click(
+	function() {
+		$("#slide").slideUp(3000);
+	}
+	);
+//click it and it fades by sliding up
 
 $("#box").click(
 	function(){
@@ -10,3 +25,5 @@ $("#box").click(
 	}
 	);
 //click event that results in an alert
+
+
